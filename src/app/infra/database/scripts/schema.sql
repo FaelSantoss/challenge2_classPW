1035701035b4c117b4c7892cb9f0a416544dce44
FaelSantoss <silva.faelm@gmail.com>
2024-05-24 16:13:18 -0300

    refactor table noticias

--- src/app/infra/database/scripts/schema.sql
+++ src/app/infra/database/scripts/schema.sql
@@ -1,11 +1,12 @@
--- Active: 1715605010850@@127.0.0.1@3306@bla
+-- Active: 1716055461921@@127.0.0.1@3306@fael
 CREATE DATABASE IF NOT EXISTS `bla`;
 
 CREATE TABLE IF NOT EXISTS noticias (
     id CHAR(36) DEFAULT(UUID()) PRIMARY KEY NOT NULL,
-    title varchar(255),
-    content varchar(255),
-    img varchar(255)
+    title VARCHAR(255),
+    content VARCHAR(255),
+    img VARCHAR(255),
+    data_criacao DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 SELECT * FROM noticias;
