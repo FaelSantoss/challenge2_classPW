32ad96fd08366bdf38d8decb319b229ffced8e7d
FaelSantoss <silva.faelm@gmail.com>
2024-05-24 16:12:37 -0300

    refactor entitie noticia

--- src/app/core/entities/noticia.py
+++ src/app/core/entities/noticia.py
@@ -1,10 +1,10 @@
 from dataclasses import dataclass
-
 from core.entities.entity import Entity
-
+from datetime import datetime
 
 @dataclass
 class Noticia(Entity):
     title: str = None
     content: str = None
     img: str = None
+    created_at: datetime = None
