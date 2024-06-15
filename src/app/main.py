1640f6e46a7865754788071aef9f09f248985911
FaelSantoss <silva.faelm@gmail.com>
2024-06-15 13:54:58 -0300

    added HTMX in form create news

--- src/app/main.py
+++ src/app/main.py
@@ -11,10 +11,15 @@ def create_app():
         __name__, template_folder="../ui/templates", static_folder="../ui/static"
     )
 
+    app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024
     app.config["SECRET_KEY"] = "1234"
     app.config['UPLOAD_FOLDER'] = 'static/uploads'
+    app.config['ALLOWED_EXTENSIONS'] = {'jpg', 'jpeg'}
     init_views(app)
 
+
+    print(f"MAX_CONTENT_LENGTH: {app.config['MAX_CONTENT_LENGTH']}", flush=True)
+    
     return app
 
 app = create_app()
